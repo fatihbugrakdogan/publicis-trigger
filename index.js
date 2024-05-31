6e59180913a977b0bc4d7fc08ee221308b19e92f
Fatih Buğra Akdoğan <omtera@Fatihs-MacBook-Pro.local>
2024-05-31 11:16:39 +0300

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import cron from "node-cron";
+import axios from "axios"; 
 
 const cronTimer = "*/5 * * * *"
 
@@ -6,6 +7,10 @@ const cronTimer = "*/5 * * * *"
 
 cron.schedule(cronTimer, async () => {
   const url = "https://publicisairtableintegration-production.up.railway.app/super-metrics"
-  const response = await fetch(url);
+  axios.get(url).then((response) => {
+    console.log(response.data);
+  }).catch((error) => {
+    console.error(error);
+  });
   console.log(response.url + " - " + response.status);
 });
