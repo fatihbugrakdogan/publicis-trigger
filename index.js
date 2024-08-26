3ead8290da458fef46d8f6ba0583a60fe45dec0d
Fatih Buğra Akdoğan <omtera@192.168.1.102>
2024-08-27 00:15:29 +0300

    webhook refresh crono added

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 import cron from "node-cron";
 import axios from "axios";
 
-const cronTimer = "*/30 * * * *";
+// const cronTimer = "*/30 * * * *";
 
-cron.schedule(cronTimer, async () => {
-  const url =
-    "https://publicisairtableintegration-production.up.railway.app/super_metrics";
-  axios
-    .get(url)
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  console.log(response.url + " - " + response.status);
-});
+// cron.schedule(cronTimer, async () => {
+//   const url =
+//     "https://publicisairtableintegration-production.up.railway.app/super_metrics";
+//   axios
+//     .get(url)
+//     .then((response) => {
+//       console.log(response.data);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+//   console.log(response.url + " - " + response.status);
+// });
 
 const cronTimerForRefreshWebhook = "0 0 * * * ";
 
